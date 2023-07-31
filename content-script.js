aacd31e2f65e8b8209fafcb70ca0c375ca5cdfd3
Yevhenii <ostrojen1996@gmail.com>
2023-07-31 11:08:47 +0300

    Add files via upload

--- content-script.js
+++ content-script.js
@@ -1,32 +1,59 @@
-function wait(selector) {
+function wait(node, selector) {
     return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+        const element = node.querySelector(selector);
+
+        if (element) {
+            resolve(element);
         }
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
+        new MutationObserver(
+            (mutations, observer) => {
+                const element = node.querySelector(selector);
+                if (element) {
+                    observer.disconnect();
+                    resolve(element);
+                }
             }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+        ).observe(
+            window.document,
+            {
+                childList: true,
+                subtree: true
+            }
+        );
     });
 }
 
-window.addEventListener('DOMContentLoaded', () => {
+function titleClean() {
+    wait(window.document, 'title').then(title => {
+        new MutationObserver(
+            (mutations, observer) => {
+                for (const mutation of mutations) {
+                    mutation.target.innerText = mutation.target.innerText.replace('/ X', '');
+                    observer.takeRecords();
+                }
+            }
+        ).observe(
+            title,
+            {
+                attributes: true,
+                childList: true,
+                subtree: true
+            }
+        );
+    })
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    titleClean();
+
     // TODO: find and replace all logo items
-    wait('h1[role=heading] svg').then(svg => {
+    wait(event.target, 'body h1[role=heading] svg').then(svg => {
         svg.parentElement.innerHTML = '<svg xml:space="preserve" viewBox="0 0 248 204"><path fill="#1d9bf0" d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07 7.57 1.46 15.37 1.16 22.8-.87-23.56-4.76-40.51-25.46-40.51-49.5v-.64c7.02 3.91 14.88 6.08 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71c25.64 31.55 63.47 50.73 104.08 52.76-4.07-17.54 1.49-35.92 14.61-48.25 20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26-3.77 11.69-11.66 21.62-22.2 27.93 10.01-1.18 19.79-3.86 29-7.95-6.78 10.16-15.32 19.01-25.2 26.16z"/></svg>';
     });
 
-    wait('link[rel="shortcut icon"]').then(link => {
-        // TODO: make it independent
-        link.href = "//abs.twimg.com/favicons/twitter.2.ico";
+    wait(event.target, 'head link[rel="shortcut icon"]').then(link => {
+        link.href = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII=';
     });
 
     // TODO: replace preview
